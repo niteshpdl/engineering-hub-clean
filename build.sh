d51e3e6d0f7883f68cd410f1c983cfe7d0b9c78e
Nitesh Paudel <niteshpaudel2004@gmail.com>
2025-04-09 09:31:14 +0545

    Update build.sh to upgrade pip first

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Update pip first
+pip install --upgrade pip
+
+# Then install requirements
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
