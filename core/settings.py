d61056df27e90ee4785e25b400ac98d4be3f9d7b
Nitesh Paudel <niteshpaudel2004@gmail.com>
2025-04-09 11:45:50 +0545

    Switch to simpler static files storage

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Crispy Forms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
